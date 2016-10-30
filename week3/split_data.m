fa77ffaa665844624a59e610f5d10720ae73b524
vsegura93 <victor.seguratir@e-campus.uab.cat>
2016-10-30 14:47:24 +0100

    split data (methods) 2

--- week3/split_data.m
+++ week3/split_data.m
@@ -14,23 +14,18 @@ function split_data(data)
         bb_1 = bb(bb~=0);
         bb_1 = bb_1';
         [n, m] = size(bb_1);
-        final_s = [];
+        bounding_box = [];
         if m> 4 
             min = 1;
             max = 4;
             bf = zeros((m/4), 4);
             for jj=1:(m/4)
-                ss = struc('x',0,'y',0,'w',0,'h',0);
                 bf(jj, 1:4) =  bb_1(1, min:max);
                 min = min + 4;
                 max = max + 4;
-                
-                ss.x = bf(jj, 1);
-                ss.y = bf(jj, 2);
-                ss.w = bf(jj, 3);
-                ss.h = bf(jj, 4);
-                
-                final_s = [final_s , ss];
+                    
+                bounding_box = [bounding_box, struct('x',bf(jj, 1),'y',bf(jj, 2),...
+                'w',bf(jj, 3),'h',bf(jj, 4));];
             end
             bb_1 = bf;
         else
@@ -39,25 +34,20 @@ function split_data(data)
            end
            
            if isempty(bb_1)
-                ss = struc('x',0,'y',0,'w',0,'h',0);
+               bounding_box = [bounding_box , struct('x',0,'y',0,'w',0,'h',0)];
            else
-               ss = struc('x',0,'y',0,'w',0,'h',0);
-               ss.x = bb_1(1, 1);
-               ss.y = bb_1(1, 2);
-               ss.w = bb_1(1, 3);
-               ss.h = bb_1(1, 4);
+               bounding_box = [bounding_box , struct('x',bb_1(1, 1),'y',bb_1(1, 2),...
+               'w',bb_1(1, 3),'h',bb_1(1, 4))];
            end
 
 
-           final_s = [final_s , ss];
         end
 
-        
         name = name(7:length(name));
         a = name(1:2);
         b = name(3:length(name));
         name = strcat(a, '.', b);
         save_dir = strcat('split_data/', name, '.mat');
-        save(save_dir, 'final_s', '-v7.3');
+        save(save_dir, 'bounding_box', '-v7.3');
     end
 end
