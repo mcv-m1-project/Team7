1b1b55b3e3ba79921a026508866960fce2bef654
vsegura93 <victor.seguratir@e-campus.uab.cat>
2016-10-30 15:43:21 +0100

    split data (methods) 2

--- week3/split_data.m
+++ week3/split_data.m
@@ -14,7 +14,7 @@ function split_data(data)
         bb_1 = bb(bb~=0);
         bb_1 = bb_1';
         [n, m] = size(bb_1);
-        bounding_box = [];
+        windowCandidates = [];
         if m> 4 
             min = 1;
             max = 4;
@@ -24,7 +24,7 @@ function split_data(data)
                 min = min + 4;
                 max = max + 4;
                     
-                bounding_box = [bounding_box, struct('x',bf(jj, 1),'y',bf(jj, 2),...
+                windowCandidates = [windowCandidates, struct('x',bf(jj, 1),'y',bf(jj, 2),...
                 'w',bf(jj, 3),'h',bf(jj, 4));];
             end
             bb_1 = bf;
@@ -34,12 +34,11 @@ function split_data(data)
            end
            
            if isempty(bb_1)
-               bounding_box = [bounding_box , struct('x',0,'y',0,'w',0,'h',0)];
+               windowCandidates = [windowCandidates , struct('x',0,'y',0,'w',0,'h',0)];
            else
-               bounding_box = [bounding_box , struct('x',bb_1(1, 1),'y',bb_1(1, 2),...
+               windowCandidates = [windowCandidates , struct('x',bb_1(1, 1),'y',bb_1(1, 2),...
                'w',bb_1(1, 3),'h',bb_1(1, 4))];
            end
-
 
         end
 
@@ -48,6 +47,6 @@ function split_data(data)
         b = name(3:length(name));
         name = strcat(a, '.', b);
         save_dir = strcat('split_data/', name, '.mat');
-        save(save_dir, 'bounding_box', '-v7.3');
+        save(save_dir, 'windowCandidates');
     end
 end
